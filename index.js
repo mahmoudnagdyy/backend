d1a9ac0d91dd9ae5a9c34e6efc5850cacb7550a0
Mahmoud Nagdy <mahmoudnagdy65@gmail.com>
2025-03-03 16:07:52 +0200

    updated

--- index.js
+++ index.js
@@ -10,6 +10,18 @@ app.use(cors({
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: 'Content-Type,Authorization'
   }));
+
+
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://login-signup-orcin.vercel.app'); // Replace this with your frontend URL
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === 'OPTIONS') {
+      res.sendStatus(200);
+    }
+  });
+
+
 app.get('/', (req, res) => {
     return res.send('Hello World!');
 })
